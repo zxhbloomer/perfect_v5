a536d861374f5ff42beb14f8d47c0f57149ded9a
zxhbloomer@gmail.com <Lyyzxh030324>
2020-08-14 11:21:33 +0800

    ..

--- perfect-core/src/main/java/com/perfect/core/config/mybatis/typehandlers/OperationFunctionInfoVoTypeHandler.java
+++ perfect-core/src/main/java/com/perfect/core/config/mybatis/typehandlers/OperationFunctionInfoVoTypeHandler.java
@@ -1,7 +1,9 @@
 package com.perfect.core.config.mybatis.typehandlers;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.perfect.bean.vo.master.rbac.permission.operation.OperationFunctionInfoVo;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
@@ -11,17 +13,19 @@ import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName: JsonHandler
- * @Description: mybatis处理JSON类型
+ * @Description: mybatis处理JSON array类型
  * @Author: zxh
  * @date: 2020/4/13
  * @Version: 1.0
  */
 @MappedTypes(value = {OperationFunctionInfoVo.class})
 @MappedJdbcTypes(value = {JdbcType.VARCHAR}, includeNullJdbcType = true)
-public class OperationFunctionInfoVoTypeHandler<T extends Object> extends BaseTypeHandler<T> {
+public class OperationFunctionInfoVoTypeHandler<T> extends BaseTypeHandler<List<T>> {
 
     private Class<T> clazz;
 
@@ -33,26 +37,49 @@ public class OperationFunctionInfoVoTypeHandler<T extends Object> extends BaseTy
     }
 
     @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, T obj, JdbcType jdbcType) throws SQLException {
-        ps.setString(i, JSON.toJSONString(obj));
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, List<T> jsonList, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setString(i,
+            JSON.toJSONString(jsonList,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteNullBooleanAsFalse,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.QuoteFieldNames,
+                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.WriteEnumUsingToString,
+                SerializerFeature.WriteClassName)
+        );
     }
 
     @Override
-    public T getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        if(rs.getString(columnName) == null){
-            return null;
-        }else {
-            return JSON.parseObject(rs.getString(columnName), clazz);
+    public List<T> getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return getJsonList(resultSet.getString(s));
+    }
+
+    @Override
+    public List<T> getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return getJsonList(resultSet.getString(i));
+    }
+
+    @Override
+    public List<T> getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return getJsonList(callableStatement.getString(i));
+    }
+
+    private List<T> getJsonList(String content) {
+        List<T> jsonResult = new ArrayList<>();
+
+        if (StringUtils.isNotBlank(content)) {
+            //            List<T> jsonList = JSON.parseObject(content, new com.alibaba.fastjson.TypeReference<List<T>>(){});
+            //            List<T> x = JSON.parseArray(content, jsonResult.getClass());
+            //            if (!NullUtil.isNull(jsonList)) {
+            //                jsonResult.addAll(jsonList);
+            //            }
+            jsonResult = JSON.parseArray(content, clazz);
+
         }
-    }
 
-    @Override
-    public T getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        return JSON.parseObject(rs.getString(columnIndex), clazz);
-    }
-
-    @Override
-    public T getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        return JSON.parseObject(cs.getString(columnIndex), clazz);
+        return jsonResult;
     }
 }
